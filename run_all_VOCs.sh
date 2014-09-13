3d1c561f2bc3727164ccde7913c9404adfc8e16d
ganna10 <janecoates10@gmail.com>
2014-09-13 09:25:40 +0200

    updated repro after .gitignore changes

---
+++ run_all_VOCs.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+perl OH_VOC_allocated_reactivity.pl CH4
+perl OH_VOC_allocated_reactivity.pl C2H6
+perl OH_VOC_allocated_reactivity.pl C3H8
+perl OH_VOC_allocated_reactivity.pl NC4H10
+perl OH_VOC_allocated_reactivity.pl IC4H10
+perl OH_VOC_allocated_reactivity.pl NC5H12
+perl OH_VOC_allocated_reactivity.pl IC5H12
+perl OH_VOC_allocated_reactivity.pl NC6H14
+perl OH_VOC_allocated_reactivity.pl NC7H16
+perl OH_VOC_allocated_reactivity.pl NC8H18
+perl OH_VOC_allocated_reactivity.pl C2H4
+perl OH_VOC_allocated_reactivity.pl C3H6
+perl OH_VOC_allocated_reactivity.pl BUT1ENE
+perl OH_VOC_allocated_reactivity.pl MEPROPENE
+perl OH_VOC_allocated_reactivity.pl C5H8
+perl OH_VOC_allocated_reactivity.pl BENZENE
+perl OH_VOC_allocated_reactivity.pl TOLUENE
+perl OH_VOC_allocated_reactivity.pl MXYL
+perl OH_VOC_allocated_reactivity.pl OXYL
+perl OH_VOC_allocated_reactivity.pl PXYL
+perl OH_VOC_allocated_reactivity.pl EBENZ
