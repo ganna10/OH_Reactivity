aec82469b1f9e2ed7ec1fc166dd02fe38262055d
ganna10 <janecoates10@gmail.com>
2014-09-20 23:23:54 +0200

    changed to stacked area plot

--- NO3_allocate_reactivity.pl
+++ NO3_allocate_reactivity.pl
@@ -84,40 +84,8 @@ foreach my $ref (@final_sorted_data) {#extract reaction and rates for each plot
 my $times = $mecca->time; #time axis
 $times -= $times->at(0);
 $times = $times(1:$ntime-2);
-$times /= 3600;
+$times /= 86400;
 my @time_axis = map { $_ } $times->dog; 
-my @time_blocks;
-foreach my $time (@time_axis) {#map to day and night
-    if ($time <= 12) {
-        push @time_blocks, "Day 1";
-    } elsif ($time > 12 and $time <= 24) {
-        push @time_blocks, "Night 1";
-    } elsif ($time > 24 and $time <= 36) {
-        push @time_blocks, "Day 2";
-    } elsif ($time > 36 and $time <= 48) {
-        push @time_blocks, "Night 2";
-    } elsif ($time > 48 and $time <= 60) {
-        push @time_blocks, "Day 3",
-    } elsif ($time > 60 and $time <= 72) {
-        push @time_blocks, "Night 3";
-    } elsif ($time > 72 and $time <= 84) {
-        push @time_blocks, "Day 4";
-    } elsif ($time > 84 and $time <= 96) {
-        push @time_blocks, "Night 4";
-    } elsif ($time > 96 and $time <= 108) {
-        push @time_blocks, "Day 5";
-    } elsif ($time > 108 and $time <= 120) {
-        push @time_blocks, "Night 5";
-    } elsif ($time > 120 and $time <= 132) {
-        push @time_blocks, "Day 6";
-    } elsif ($time > 132 and $time <= 144) {
-        push @time_blocks, "Night 6";
-    } elsif ($time > 144 and $time <= 156) {
-        push @time_blocks, "Day 7";
-    } else {
-        push @time_blocks, "Night 7";
-    }
-}
 
 my $R = Statistics::R->new();
 $R->run(q` library(ggplot2) `,
@@ -127,18 +95,17 @@ $R->run(q` library(ggplot2) `,
         q` library(grid) `,
 );
 
-$R->set('Time', [@time_blocks]);
+$R->set('Time', [@time_axis]);
 $R->run(q` data = data.frame(Time) `);
 foreach my $ref (@plot_data) {
     foreach my $process (sort keys %$ref) {
         $R->set('Process', $process);
         $R->set('Reactivity', [@{$ref->{$process}}]);
-        $R->run(q` data[Process] = Reactivity `);
+        $R->run(q` data[Process] = Reactivity * 1e2 `);
     }
 }
 
-$R->run(q` data = ddply(data, .(Time), colwise(sum)) `, #arrange data
-        q` data = melt(data, id.vars = c("Time"), variable.name = "Process", value.name = "Reactivity") `,
+$R->run(q` data = melt(data, id.vars = c("Time"), variable.name = "Process", value.name = "Reactivity") `,
         q` data$Process = factor(data$Process, levels = rev(levels(data$Process))) `,
 );
 
@@ -190,11 +157,12 @@ $R->run(q` my.colours = c(  "CO" = "#2b9eb3" ,
 );
 
 $R->run(q` plot = ggplot(data = data, aes(x = Time, y = Reactivity, fill = Process)) `, #plot data
-        q` plot = plot + geom_bar(stat = "identity", width = 0.7) `,
-        q` plot = plot + scale_x_discrete(limits = c("Day 1", "Night 1", "Day 2", "Night 2", "Day 3", "Night 3", "Day 4", "Night 4", "Day 5", "Night 5", "Day 6", "Night 6", "Day 7", "Night 7")) `,
-        q` plot = plot + scale_y_continuous(limits = c(0, 1.5), breaks = seq(0, 1.5, 0.5)) `,
+        q` plot = plot + geom_area(position = "stack") `,
+        q` plot = plot + geom_area(position = "stack", colour = "black", show_guide = FALSE) `,
+        q` plot = plot + scale_x_continuous(limits = c(0, 7), breaks = seq(0, 7, 1)) `,
+        q` plot = plot + scale_y_continuous(limits = c(0, 9), breaks = seq(0, 9, 1)) `,
         q` plot = plot + ggtitle("Allocated NO3 Reactivity without NOx and HOx Contributions") `,
-        q` plot = plot + ylab(expression(bold(paste("Reactivity (", s^-1, ")")))) `,
+        q` plot = plot + ylab(expression(bold(paste("Reactivity (", s^-1, ") x ", 10^2)))) `,
         q` plot = plot + theme_bw() `,
         q` plot = plot + theme(plot.title = element_text(size = 32, face = "bold")) `,
         q` plot = plot + theme(axis.title.x = element_blank()) `,
